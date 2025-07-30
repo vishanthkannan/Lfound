c48b1c0a63c6f06129b92d9ec764ef1a01d430fa
vishanthannan <vishanthkannan777@gmail.com>
2025-07-30 23:23:56 +0530

    commit-1

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -100,7 +100,7 @@ const Login = ({ onLogin }) => {
 
               <div className="text-center mt-3">
                 <p className="mb-0">
-                  Have an account?{' '}
+                  Don't have an account?{' '}
                   <a href="/signup" className="text-primary">Sign up</a>
                 </p>
               </div>
